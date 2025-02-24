e8d5c6daa558937d723123b9407452bec8af3561
Swati Gupta <swatig.dev@gmail.com>
2025-02-24 12:54:55 +0530

    frontend initial setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <main className="max-w-xl mx-auto px-4 py-8"> 
+        <h1 className="text-5xl font-bold">Byte++</h1>
+        <h2 className="text-4xl">The Coding Society of BHU</h2>
+      </main>
+    </>
+  )
+}
+
+export default App
