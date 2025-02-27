ed3ade34f96eb96e549225a408d6c63c659ace21
rukmana-2003 <132250404+rukmana-2003@users.noreply.github.com>
2025-02-28 01:17:58 +0530

    Zoom Effect Added to Card

--- frontend/src/pages/Cards.jsx
+++ frontend/src/pages/Cards.jsx
@@ -1,6 +1,6 @@
 import CardBack from "../assets/CardBack.jpg"
 import Portfolio from "../assets/Portfolio.png"
-import CodingCompition from "../assets/CodingCompitation.png"
+import CodingCompetition from "../assets/CodingCompetition.png"
 import {Link} from "react-router-dom"
 
 
@@ -12,13 +12,13 @@ export const Cards = () => {
           <h1 className="text-3xl text-center font-bold py-15">Unlocking Cosmic Potential</h1>
           <div className="cards flex gap-24 justify-center ">
             <div className="card">
-              <Link to=""><div className="h-72 shadow-2xl shadow-purple-600 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${CodingCompition})`,backgroundSize:`cover`,boxShadow: `rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
+              <Link to=""><div className="h-72 shadow-2xl transform transition-transform duration-300 hover:scale-110 shadow-purple-600 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${CodingCompetition})`,backgroundSize:`cover`,boxShadow: `rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
             </div>
             <div className="card">
-              <Link to=""><div className="h-72 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${Portfolio})`,backgroundSize:`cover`,boxShadow:`rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
+              <Link to=""><div className="h-72 w-60 rounded-2xl transform transition-transform duration-300 hover:scale-110 bg-amber-50" style={{backgroundImage: `url(${Portfolio})`,backgroundSize:`cover`,boxShadow:`rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
             </div>
             <div className="card">
-              <Link to=""><div className="h-72 shadow-2xl shadow-purple-600 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${CodingCompition})`,backgroundSize:`cover`,boxShadow:`rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
+              <Link to=""><div className="h-72 shadow-2xl transform transition-transform duration-300 hover:scale-110 shadow-purple-600 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${CodingCompetition})`,backgroundSize:`cover`,boxShadow:`rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
             </div>
           </div>
         </div>
