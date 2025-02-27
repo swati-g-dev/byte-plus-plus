a5af39da0830f20b3d203b64a054853f6a38e4a6
rukmana-2003 <132250404+rukmana-2003@users.noreply.github.com>
2025-02-28 00:37:25 +0530

    Cards Page Done

--- frontend/src/pages/Cards.jsx
+++ frontend/src/pages/Cards.jsx
@@ -1,5 +1,28 @@
+import CardBack from "../assets/CardBack.jpg"
+import Portfolio from "../assets/Portfolio.png"
+import CodingCompition from "../assets/CodingCompitation.png"
+import {Link} from "react-router-dom"
+
+
 export const Cards = () => {
   return (
-    <div>Cards</div>
+    <div>
+      <div className="cards-parent">
+        <div className="card-back h-[90vh] w-[100%]" style={{backgroundImage: `url(${CardBack})`,backgroundPosition:`center`,backgroundSize:`cover`}}>
+          <h1 className="text-3xl text-center font-bold py-15">Unlocking Cosmic Potential</h1>
+          <div className="cards flex gap-24 justify-center ">
+            <div className="card">
+              <Link to=""><div className="h-72 shadow-2xl shadow-purple-600 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${CodingCompition})`,backgroundSize:`cover`,boxShadow: `rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
+            </div>
+            <div className="card">
+              <Link to=""><div className="h-72 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${Portfolio})`,backgroundSize:`cover`,boxShadow:`rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
+            </div>
+            <div className="card">
+              <Link to=""><div className="h-72 shadow-2xl shadow-purple-600 w-60 rounded-2xl bg-amber-50" style={{backgroundImage: `url(${CodingCompition})`,backgroundSize:`cover`,boxShadow:`rgb(249 174 240 / 96%) 0px 0px 15px 15px`}}></div></Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
