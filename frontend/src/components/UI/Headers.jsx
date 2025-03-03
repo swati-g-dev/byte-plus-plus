bdd5834ab2a1a526089a4ddd87866b1dff2adc82
Swati Gupta <swatig.dev@gmail.com>
2025-03-04 01:38:30 +0530

    fixed logo

--- frontend/src/components/UI/Headers.jsx
+++ frontend/src/components/UI/Headers.jsx
@@ -2,14 +2,13 @@ import { NavLink } from "react-router-dom"
 
 export const Headers = () => {
   return (
-    <header className="h-20 bg-opacity-80 bg-gray-900">
+    <header className="h-20 bg-gray-900">
       <div className="max-w-[90%] mx-auto flex items-center justify-between h-full">
         
         <div className="relative w-16 h-16">
-          <div className="absolute w-full h-full border-2 border-white rounded-full"
-> </div>
+          <div className="absolute w-full h-full border-2 border-white rounded-full"></div>
             <NavLink to="/">
-            <img src="/Logo.jpeg" alt="Logo" className="w-full h-full rounded-full"/></NavLink>
+            <img src={`${import.meta.env.BASE_URL}Logo.jpeg`} alt="Logo" className="w-full h-full rounded-full"/></NavLink>
           </div>
 
           <nav>
@@ -22,18 +21,11 @@ export const Headers = () => {
             </ul>
           </nav>
 
-          <div className="flex gap-4">
-          <button className="bg-indigo-400 hover:bg-indigo-500">Login</button>
+          <div className="flex font-medium gap-4">
+          <button className="bg-indigo-500 hover:bg-indigo-600">Login</button>
           <button className="text-gray-300 bg-gray-500 hover:bg-gray-600">Sign Up</button>
         </div>
         </div>
     </header>
-    // <header className="flex items-center justify-between px-8 py-4 bg-opacity-80 bg-gray-900">
-    //       <div className="relative w-12 h-12">
-    //         <div className="absolute animate-spin w-full h-full border border-white rounded-full">
-    //           <span className="block text-lg font-bold">Byte++</span>
-    //         </div>
-    //       </div>
-    //     </header>
   )
 }
